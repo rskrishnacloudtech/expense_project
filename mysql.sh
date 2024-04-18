dc197e5a80e0972eae036d63d5c527afe53b589b
Siva <rsk.sfdctraining1@gmail.com>
2024-04-18 22:39:30 +0530

    Expense Project: MySQL Server Configuration

--- mysql.sh
+++ mysql.sh
@@ -22,10 +22,10 @@ N="\e[0m"
 VALIDATE(){
     if [ $1 -ne 0 ]
     then
-        echo -e "$2.. installation of $2 is $R FAILED $N"
+        echo -e "$2 is $R FAILED $N"
         exit 1
     else
-        echo -e "$2.. installation of $2 is $G SUCCESS $N"
+        echo -e "$2 is $G SUCCESS $N"
     fi
 }
 
