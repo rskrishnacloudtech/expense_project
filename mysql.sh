7d461cc3a2c6bbbdd4eb14d9afbdf31572da096b
Siva <rsk.sfdctraining1@gmail.com>
2024-04-22 08:32:17 +0530

    Expense Project: MySQL Server Configuration

--- mysql.sh
+++ mysql.sh
@@ -56,7 +56,7 @@ VALIDATE $? "Starting the mysql service"
 
 # Setting the password to the root user of mysql server. If the password is already set it will set again. So we need to check its first and 
 # then we have to set the password if its not set already.
-mysql -h DBServerIP -uroot -p$mySQLPassword -e "show databases;" &>> $LOGFILE
+mysql -h $DBServerIP -uroot -p$mySQLPassword -e "show databases;" &>> $LOGFILE
 if [ $? -eq 0 ]
 then
     echo -e "MySQL root password is already set. $Y SKIPPING $N"
